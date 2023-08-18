8966be6cda5eece71871a15fda53aa15a4e60543
chibuzodaniel <chibuzodaniel71@gmail.com>
2023-08-18 08:20:18 +0100

    update

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-function startLoading() {
-    const loadingContainer = document.querySelector('.loading-container');
-    const loadButton = document.getElementById('loadButton');
+function updateDateTime() {
+  const currentDate = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  const formattedDate = currentDate.toLocaleDateString(undefined, options);
   
-    // Show the loading spinner and disable the button
-    loadingContainer.style.display = 'block';
-    loadButton.disabled = true;
+  document.getElementById("currentDateTime").textContent = formattedDate;
+}
+
+// Update the current time and date every second
+// setInterval(updateDateTime, 1000);
+
+// Initial call to update the time and date
+updateDateTime();
+
+
+function updateCurrentDate() {
+  const currentDate = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = currentDate.toLocaleDateString(undefined, options);
   
-    // Simulate loading process (you can replace this with your actual loading tasks)
-    setTimeout(function() {
-      // Hide the loading spinner and enable the button after a delay (e.g., 3 seconds)
-      loadingContainer.style.display = 'none';
-      loadButton.disabled = false;
-    }, 3000); // Simulate a 3-second loading process
-  }
-  
+  document.getElementById("currentDate").textContent = formattedDate;
+}
+
+// Call the function to update the current date
+updateCurrentDate();
